59783b3e42f690a8062b2056944f318822eeff86
darshnablah <dwright758@gmail.com>
2020-06-21 18:57:12 -0500

    some js created

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,52 @@
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-var item = "";
+var items = {};
+var textArea = $("<textarea>").addClass("form-control");
+var item = $(".item");
 
+
+function color (time) {
+    var time = item;
+    var elem = $(".hour").text();
+
+    time.children().each().removeClass("past present future");
+    
+    console.log(time);
+
+    if (moment().isAfter(elem)) {
+        $(this).children().each().addClass("past");
+    }
+    else if (moment().isBefore(elem)) {
+        $(this).children().each().addClass("future");
+    }
+    else {
+        $(this).children().each().addClass("present");
+    }
+    console.log(this);
+}
+setInterval(color, 1000 * 60);
+
+
+$(".item").on("change", function () {
+    items = $(this).val().trim();
+    console.log(items);
+});
+
+
+
+var loadItems = function () {
+    
+    item.append(textArea);
+
+    items = JSON.parse(localStorage.getItem("item"));
+
+    color();
+}
+
+$(".saveBtn").on("click", function () {
+    localStorage.setItem("item", JSON.stringify(items));
+});
+
+
+loadItems ();
